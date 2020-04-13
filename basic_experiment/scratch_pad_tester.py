164bd8367375db69452fb90d93478bed79519640
LRNavin <lr.navin@yahoo.com>
2020-04-13 16:01:24 +0200

    scripts done

--- basic_experiment/scratch_pad_tester.py
+++ basic_experiment/scratch_pad_tester.py
@@ -1,10 +1,17 @@
 import numpy as np
 from scipy import stats
+from distutils.dir_util import copy_tree
+import shutil
 
-a = np.array([[6, 8, 3, 0],
-               [3, 2, 1, 7],
-               [8, 1, 8, 4],
-               [5, 3, 0, 5],
-               [4, 7, 5, 9]])
-m=stats.mode(a)
-print(m.mode[0])
+
+
+# copy_tree(fromDirectory, toDirectory)
+# a = np.array([[6, 8, 3, 0],
+# #                [3, 2, 1, 7],
+# #                [8, 1, 8, 4],
+# #                [5, 3, 0, 5],
+# #                [4, 7, 5, 9]])
+# # m=stats.mode(a)
+# # print(m.mode[0])
+
+
